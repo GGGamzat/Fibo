fec7178a5d7a697e9e3a6d77731b4d69b4d0cfbb
Гамзат <abdullayevgamzat@gmail.com>
2025-08-26 23:13:43 +0300

    create block novelty

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,12 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import slide1 from '../assets/img/1.jpg';
-import slide2 from '../assets/img/2.jpg';
-import slide3 from '../assets/img/3.jpg';
-import slide4 from '../assets/img/4.png';
-import slide5 from '../assets/img/5.jpg';
-import slide6 from '../assets/img/6.jpg';
+import slide1 from '../assets/img/slider_img/1.jpg';
+import slide2 from '../assets/img/slider_img/2.jpg';
+import slide3 from '../assets/img/slider_img/3.jpg';
+import slide4 from '../assets/img/slider_img/4.png';
+import slide5 from '../assets/img/slider_img/5.jpg';
+import slide6 from '../assets/img/slider_img/6.jpg';
 
 // Import Swiper styles
 import 'swiper/css';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
 import Header from '../components/Header.jsx';
 import Slider from '../components/Slider.jsx';
+import noveltyList from '../data/data.js';
 
 function Home() {
     return (
         <>
             <Header></Header>
             <Slider></Slider>
+            <main className="main">
+                <div className="main__container">
+                    <div className="novelty">
+                        <span className="novelty__title">Новинки</span>
+                        <div className="novelty__block">
+                            {noveltyList.map(({ img, title, price }) => (
+                                <div className="novelty__cart">
+                                    <img src={ img } alt="" className="novelty__cart-img" />
+                                    <div className="novelty__cart-info">
+                                        <h3 className="novelty__cart-title">{title}</h3>
+                                        <h3 className="novelty__cart-price">{price} ₽</h3>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </main>
         </>
     );
 }
